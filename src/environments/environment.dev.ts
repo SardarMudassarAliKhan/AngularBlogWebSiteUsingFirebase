a744767d9c99e6b56a02f75df7bc8b2f698b3500
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-03-27 06:56:58 +0500

    refact: Code Refactorization for Optming the Cloud API Keys
    Ticket #1003

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-  apiKey: "AIzaSyCv04lpKENw7TG0g5YX53e6wPEvJIOIxHw",
-  authDomain: "blogsite-85dff.firebaseapp.com",
-  projectId: "blogsite-85dff",
-  storageBucket: "blogsite-85dff.appspot.com",
-  messagingSenderId: "50890606086",
-  appId: "1:50890606086:web:2db8ddc689dbb84b44e587",
-  measurementId: "G-XVNLJM5ZZT"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
   }
 };
